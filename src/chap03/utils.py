b429f7ab7f414516b97da4783e44b9953c6eff3b
Tokuma Suzuki <tokuma.suzuki09@gmail.com>
2021-11-06 07:41:13 +0000

    add utility

--- src/chap03/utils.py
+++ src/chap03/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 
 def load_UK_article():
@@ -8,3 +9,16 @@ def load_UK_article():
         df = pd.read_json('../../data/chap03/jawiki-country.json', lines=True)
 
     return df[df['title'] == 'イギリス']['text'].values[0]
+
+
+def remove_emphasis(res):
+    return {key: re.sub(r'\'+', '', value) for key, value in res.items()}
+
+
+def remove_inner_link(res):
+    # [[記事名]]
+    # [[記事名|表示文字]]
+    # 表示文字がなければ記事名を利用する。
+    # 最初の()は記事名|があれば記事名|に、そうでなければ、空文字にマッチさせる
+    pattern = re.compile(r'\[\[(.*\||)(.*)\]\]')
+    return {key: re.sub(pattern, r'\2', value) for key, value in res.items()}
